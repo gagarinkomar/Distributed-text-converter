6ef0501f482385d959c6d5852a974f121223d6e5
gagarinkomar <gagarinkomar@gmail.com>
2024-12-20 03:28:36 +0300

    Add image edit task

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,25 +1,26 @@
-from requests.models import Request
+from requests.models import Request, UploadedFile, EditedFile
 from time import sleep
 from requests.cutom_image_handler import ImageHandler
 
 from backend.celery import app
+
 
 @app.task
 def task1(number):
     res = Request.objects.create(test_field=number)
     return True
 
-# Изменить изображение 
+# Изменить изображение
+
+
 @app.task
-def task2(request_id, file_id):
+def task2(file_id):
+    image = UploadedFile.get_by_id(file_id)
+
     image_handler = ImageHandler()
-    image = image_handler.download_from_s3(str(request_id) + '/' + str(file_id))
     
-    edited_image = image_handler.edit(image)
-    
-    image_handler.upload_to_s3(
-        file_name=str(request_id) + '/' + str(file_id),
-        file_content=edited_image
-    )
-    
+    edited_image = image_handler.edit(image.get_file_data())
+
+    EditedFile.create_file(image.request, image.uploaded_name, edited_image)
+
     return True
